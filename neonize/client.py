b561fce3cf24fa58cc5260041cb73de9882baf86
aqasemi <59507561+aqasemi@users.noreply.github.com>
2025-06-18 00:02:51 +0000

    Whatsmeow - Update proto files - Update golang depedencies

--- neonize/client.py
+++ neonize/client.py
@@ -158,7 +158,7 @@ from .utils.enum import (
     PrivacySetting,
     PrivacySettingType,
     VoteType,
-    MediaTypeToMMS
+    MediaTypeToMMS,
 )
 from .utils.ffmpeg import FFmpeg
 from .utils.iofile import get_bytes_from_name_or_url
@@ -1421,8 +1421,7 @@ class NewClient:
         jidbuf = JIDArray(JIDS=jid).SerializeToString()
         JIDS = self.__client.GetJIDFromLID(self.uuid, jidbuf, len(jidbuf)).get_bytes()
         model = JIDArray.FromString(JIDS)
-        return model.JIDS        
-
+        return model.JIDS
 
     def get_group_info(self, jid: JID) -> GroupInfo:
         """Retrieves information about a group.
@@ -2672,7 +2671,10 @@ class ClientFactory:
         return self.get_all_devices_from_db(self.database_name)
 
     def new_client(
-        self, jid: Optional[JID] = None, uuid: Optional[str] = None, props: Optional[DeviceProps] = None
+        self,
+        jid: Optional[JID] = None,
+        uuid: Optional[str] = None,
+        props: Optional[DeviceProps] = None,
     ) -> NewClient:
         """
         This function creates a new instance of the client. If the jid parameter is not provided, a new client will be created.
